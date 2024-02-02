c4672fc288d4fd4cd580302632a140be799f207a
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2024-02-02 10:42:31 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -52,6 +52,13 @@ module.exports = {
             filename: 'style.css'
         }),
         new CleanWebpackPlugin(),
-        new Dotenv()
+        new Dotenv({
+            path: path.resolve(process.cwd(), '.env'),
+            safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
+            allowEmptyValues: true, // allow empty variables (e.g. `FOO=`) (treat it as empty string, rather than missing)
+            systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+            silent: true, // hide any errors
+            defaults: false, // load '.env.defaults' as the default values if empty.
+        })
     ]
 }
